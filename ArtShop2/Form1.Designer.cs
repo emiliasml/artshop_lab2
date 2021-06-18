a76c673c7fe7ffc97af593907a613a65dc4a5752
Emilia <emiliasml@yahoo.com>
2021-06-18 18:09:36 +0300

    Lab 1 made more generic

---
+++ ArtShop2/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace ArtShop2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.lblParent = new System.Windows.Forms.Label();
+            this.lblChild = new System.Windows.Forms.Label();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.reloadBtn = new System.Windows.Forms.Button();
+            this.insertBtn = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.updateBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(956, 266);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(22, 356);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 28;
+            this.dataGridView2.Size = new System.Drawing.Size(956, 234);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellClick);
+            this.dataGridView2.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellDoubleClick);
+            // 
+            // lblParent
+            // 
+            this.lblParent.AutoSize = true;
+            this.lblParent.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblParent.Location = new System.Drawing.Point(405, 8);
+            this.lblParent.Name = "lblParent";
+            this.lblParent.Size = new System.Drawing.Size(136, 33);
+            this.lblParent.TabIndex = 1;
+            this.lblParent.Text = "lblParent";
+            // 
+            // lblChild
+            // 
+            this.lblChild.AutoSize = true;
+            this.lblChild.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChild.Location = new System.Drawing.Point(414, 322);
+            this.lblChild.Name = "lblChild";
+            this.lblChild.Size = new System.Drawing.Size(116, 33);
+            this.lblChild.TabIndex = 1;
+            this.lblChild.Text = "lblChild";
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Font = new System.Drawing.Font("Montserrat Medium", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteBtn.Location = new System.Drawing.Point(993, 416);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(361, 54);
+            this.deleteBtn.TabIndex = 2;
+            this.deleteBtn.Text = "DELETE";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // reloadBtn
+            // 
+            this.reloadBtn.Font = new System.Drawing.Font("Montserrat Medium", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reloadBtn.Location = new System.Drawing.Point(993, 536);
+            this.reloadBtn.Name = "reloadBtn";
+            this.reloadBtn.Size = new System.Drawing.Size(361, 54);
+            this.reloadBtn.TabIndex = 3;
+            this.reloadBtn.Text = "REFRESH";
+            this.reloadBtn.UseVisualStyleBackColor = true;
+            this.reloadBtn.Click += new System.EventHandler(this.reloadBtn_Click);
+            // 
+            // insertBtn
+            // 
+            this.insertBtn.Font = new System.Drawing.Font("Montserrat Medium", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertBtn.Location = new System.Drawing.Point(993, 356);
+            this.insertBtn.Name = "insertBtn";
+            this.insertBtn.Size = new System.Drawing.Size(361, 54);
+            this.insertBtn.TabIndex = 4;
+            this.insertBtn.Text = "INSERT";
+            this.insertBtn.UseCompatibleTextRendering = true;
+            this.insertBtn.UseVisualStyleBackColor = true;
+            this.insertBtn.Click += new System.EventHandler(this.insertBtn_Click);
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(993, 42);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(361, 266);
+            this.panel.TabIndex = 5;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Font = new System.Drawing.Font("Montserrat Medium", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updateBtn.Location = new System.Drawing.Point(993, 476);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(361, 54);
+            this.updateBtn.TabIndex = 6;
+            this.updateBtn.Text = "UPDATE";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1388, 639);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.insertBtn);
+            this.Controls.Add(this.reloadBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.lblChild);
+            this.Controls.Add(this.lblParent);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label lblParent;
+        private System.Windows.Forms.Label lblChild;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Button reloadBtn;
+        private System.Windows.Forms.Button insertBtn;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button updateBtn;
+    }
+}
+
---
+++ ArtShop2/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace ArtShop2
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = "Server=DESKTOP-EMSML\\SQLEXPRESS; Database=ArtShop;Integrated Security=true;";
+        DataSet ds = new DataSet();
+        SqlDataAdapter parentAdapter = new SqlDataAdapter();
+        SqlDataAdapter childAdapter = new SqlDataAdapter();
+        BindingSource bsParent = new BindingSource();
+        BindingSource bsChild = new BindingSource();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Art Shop for broke artists";
+            lblParent.Text = ConfigurationManager.AppSettings["parent"];
+            lblChild.Text = ConfigurationManager.AppSettings["child"];
+            deleteBtn.BackColor = Color.LightSlateGray;
+            insertBtn.BackColor = Color.LightGray;
+            reloadBtn.BackColor = Color.LightGray;
+            updateBtn.BackColor = Color.LightGray;
+            panel.BackColor = Color.LightGray;
+            gridFill();
+            // MessageBox.Show(ConfigurationManager.AppSettings["greeting"]);
+        }
+
+
+        void gridFill()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    parentAdapter.SelectCommand = new SqlCommand(
+                        ConfigurationManager.AppSettings["selectParent"], connection);
+                    childAdapter.SelectCommand = new SqlCommand(
+                        ConfigurationManager.AppSettings["selectChild"], connection);
+                    parentAdapter.Fill(ds, ConfigurationManager.AppSettings["parent"]);
+                    childAdapter.Fill(ds, ConfigurationManager.AppSettings["child"]);
+                    bsParent.DataSource = ds.Tables[ConfigurationManager.AppSettings["parent"]];
+                    DataColumn pk = ds.Tables[ConfigurationManager.AppSettings["parent"]].Columns[
+                        ConfigurationManager.AppSettings["pk"]];
+                    DataColumn fk = ds.Tables[ConfigurationManager.AppSettings["child"]].Columns[
+                        ConfigurationManager.AppSettings["fk"]];
+                    DataRelation relation = new DataRelation("FK_Parent_Child", pk, fk);
+                    ds.Relations.Add(relation);
+                    bsChild.DataSource = bsParent;
+                    bsChild.DataMember = "FK_Parent_Child";
+                    dataGridView1.DataSource = bsParent;
+                    dataGridView2.DataSource = bsChild;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void reload()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    ds.Clear();
+                    parentAdapter.SelectCommand = new SqlCommand(
+                        ConfigurationManager.AppSettings["selectParent"], connection);
+                    childAdapter.SelectCommand = new SqlCommand(
+                        ConfigurationManager.AppSettings["selectChild"], connection);
+                    parentAdapter.Fill(ds, ConfigurationManager.AppSettings["parent"]);
+                    childAdapter.Fill(ds, ConfigurationManager.AppSettings["child"]);
+                    bsParent.DataSource = ds.Tables[ConfigurationManager.AppSettings["parent"]];
+                    bsChild.DataSource = bsParent;
+                    dataGridView1.DataSource = bsParent;
+                    dataGridView2.DataSource = bsChild;
+                    panel.Controls.Clear();
+                    insertBtn.Visible = deleteBtn.Visible = updateBtn.Visible = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void generateTextBoxesForINSERT()
+        {
+            deleteBtn.Visible = updateBtn.Visible = false;
+            panel.Controls.Clear();
+            insertBtn.Visible = true;
+            List<string> childColumns = new List<string>(ConfigurationManager.AppSettings["childColumns"].Split('/'));
+            panel.Controls.Clear();
+            int y = 50;
+            Label title = new Label();
+            title.Location = new Point(100, 25);
+            title.Parent = panel;
+            title.Text = "INSERT";
+            foreach (string column in childColumns)
+            {
+                TextBox txtBox = new TextBox();
+                txtBox.Location = new Point(110, y);
+                txtBox.Name = column;
+                txtBox.Parent = panel;
+                Label label = new Label();
+                label.Location = new Point(10, y);
+                label.Text = column;
+                label.Parent = panel;
+                y = y + 25;
+
+            }
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    DialogResult dialog;
+                    dialog = MessageBox.Show("Are you sure?", "Please confirm this operation", MessageBoxButtons.YesNoCancel);
+                    if (dialog == DialogResult.Yes)
+                    {
+                        string childId = dataGridView2.CurrentRow.Cells[
+                            ConfigurationManager.AppSettings["idChild"]].Value.ToString();
+                        childAdapter.DeleteCommand = new SqlCommand(
+                            ConfigurationManager.AppSettings["delete"], connection);
+                        childAdapter.DeleteCommand.Parameters.Add("@Id", SqlDbType.Int).Value = childId;
+                        childAdapter.DeleteCommand.Connection = connection;
+                        childAdapter.DeleteCommand.ExecuteNonQuery();
+                        MessageBox.Show("Deleted successfully!");
+                        reload();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void reloadBtn_Click(object sender, EventArgs e)
+        {
+            reload();
+        }
+
+        private void insertBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    childAdapter.InsertCommand = new SqlCommand(ConfigurationManager.AppSettings["insert"], connection);
+                    List<string> stringChildColumns = new List<string>(ConfigurationManager.AppSettings["stringChildColumns"].Split('/'));
+                    List<string> floatChildColumns = new List<string>(ConfigurationManager.AppSettings["floatChildColumns"].Split('/'));
+                    List<string> intChildColumns = new List<string>(ConfigurationManager.AppSettings["intChildColumns"].Split('/'));
+
+
+                    string parentId = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+
+                    childAdapter.InsertCommand.Parameters.Add(
+                        ConfigurationManager.AppSettings["idParentinChildTable"], SqlDbType.Int).Value = parentId;
+
+                    foreach (string column in stringChildColumns)
+                    {
+                        childAdapter.InsertCommand.Parameters.Add("@" + column, SqlDbType.VarChar).Value = panel.Controls[column].Text;
+                    }
+
+                    if (floatChildColumns[0] != "")
+                    {
+                        foreach (string column in floatChildColumns)
+                        {
+                            childAdapter.InsertCommand.Parameters.Add("@" + column, SqlDbType.Float).Value = float.Parse(panel.Controls[column].Text);
+                        }
+                    }
+
+                    if (intChildColumns[0] != "")
+                    {
+                        foreach (string column in intChildColumns)
+                        {
+                            childAdapter.InsertCommand.Parameters.Add("@" + column, SqlDbType.Int).Value = Int32.Parse(panel.Controls[column].Text);
+                        }
+                    }
+
+                    childAdapter.InsertCommand.Connection = connection;
+                    childAdapter.InsertCommand.ExecuteNonQuery();
+                    MessageBox.Show("ADDED!");
+                    reload();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            generateTextBoxesForINSERT();
+        }
+
+        private void dataGridView2_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            updateBtn.Visible=true;
+            deleteBtn.Visible = insertBtn.Visible = false;
+            panel.Controls.Clear();
+            
+            List<string> childColumns = new List<string>(ConfigurationManager.AppSettings["childColumnsUpdate"].Split('/'));
+            int y = 50;
+            Label title = new Label();
+            title.Location = new Point(100, 25);
+            title.Parent = panel;
+            title.Text = "UPDATE";
+            foreach (string column in childColumns)
+            {
+                TextBox txtBox = new TextBox();
+                txtBox.Location = new Point(110, y);
+                txtBox.Name = column;
+                txtBox.Parent = panel;
+                Label label = new Label();
+                label.Location = new Point(10, y);
+                label.Text = column;
+                label.Parent = panel;
+                y = y + 25;
+                txtBox.Clear();
+                txtBox.Text = dataGridView2.CurrentRow.Cells[column].Value.ToString();
+            }
+
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    childAdapter.UpdateCommand = new SqlCommand(
+                        ConfigurationManager.AppSettings["update"], connection);
+                    List<string> stringChildColumns = new List<string>(ConfigurationManager.AppSettings["stringChildColumns"].Split('/'));
+                    List<string> floatChildColumns = new List<string>(ConfigurationManager.AppSettings["floatChildColumns"].Split('/'));
+                    List<string> intChildColumns = new List<string>(ConfigurationManager.AppSettings["intChildColumns"].Split('/'));
+
+                    string childId = dataGridView2.CurrentRow.Cells[0].Value.ToString();
+
+                    childAdapter.UpdateCommand.Parameters.Add(
+                        ConfigurationManager.AppSettings["idChild"], SqlDbType.Int).Value = childId;
+
+                    foreach (string column in stringChildColumns)
+                    {
+                        childAdapter.UpdateCommand.Parameters.Add("@" + column, SqlDbType.VarChar).Value = panel.Controls[column].Text;
+                    }
+
+                    if (floatChildColumns[0] != "")
+                    {
+                        foreach (string column in floatChildColumns)
+                        {
+                            childAdapter.UpdateCommand.Parameters.Add("@" + column, SqlDbType.Float).Value = float.Parse(panel.Controls[column].Text);
+                        }
+                    }
+
+                    if (intChildColumns[0] != "")
+                    {
+                        foreach (string column in intChildColumns)
+                        {
+                            childAdapter.UpdateCommand.Parameters.Add("@" + column, SqlDbType.Int).Value = Int32.Parse(panel.Controls[column].Text);
+                        }
+                    }
+                    string parentId = panel.Controls[ConfigurationManager.AppSettings["idParentinChildTable"]].Text;
+                    childAdapter.UpdateCommand.Parameters.Add("@"+ ConfigurationManager.AppSettings["idParentinChildTable"],
+                        SqlDbType.Int).Value = Int32.Parse(parentId);
+                    childAdapter.UpdateCommand.Connection = connection;
+                    childAdapter.UpdateCommand.ExecuteNonQuery();
+                    reload();
+                    MessageBox.Show("UPDATED!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            deleteBtn.Visible = true;
+            updateBtn.Visible = insertBtn.Visible = false;
+            panel.Controls.Clear();
+            panel.Controls.Clear();
+        }
+    }
+}
